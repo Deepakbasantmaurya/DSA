407595770da392e4ae1ee4bc9d73e36565fb5aaa
DEEPAK MAURYA <146301775+Deepakbasantmaurya@users.noreply.github.com>
2024-10-08 10:10:03 +0530

    Create Additon of star and end index.c

---
+++ Additon of star and end index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a[]={16,12,13,14,15 },i,j;
+    for( i=0,j=4;i<j;i++,j--){
+          printf ("%d\n",a[i]+a[j]);
+          }
+          if(i==j)
+             printf ("%d\n",a[i]);
+      
+    
+
+    return 0;
+}
